9ae1c8aa583cc2d67d1b8e5ab6d92f5481e71aa9
Hafsa Egal <hafsaegal915@gmail.com>
2025-03-19 02:56:48 +0300

    m

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-const form=document.getElementById("signupForm")
- form.addEventListener("submit", (e)=>{
-    e.preventDefault();
+const form=document.getElementById("signupForm");
+const postForm=document.querySelector("#post-form" )
+const imageSelector= document.querySelector("input[type='file']")
+const imagePreview= document.querySelector("#img-preview")
+ form.addEventListener("submit", (e)=>{})
+ 
+    //e.preventDefault();
+console.log(imageSelector)
+    
+    imageSelector.addEventListener("change",(e)=>{
+        console.log(e.target.files)
+        //file reader:
+        const file=e.target.files[0];
+        const fr = new FileReader();
+
+        fr.onload= (evt)=> {
+            imagePreview.src= evt.target.result
+        }
+        fr.readAsDataURL(file)
+    })
+
+    postForm.addEventListener("submit", (e) => {});
     // const fd= new FormData (form)
     // console.log(fd.enteries)
 
-    const jsonObject= {
-        name: form.name.value,
-        email: form.email.value,
-        gender:form.gender.value,
-        DOB:form.DOB.value,
-        password:form.password.value
-    }
-    console.log(jsonObject)
+//     const jsonObject= {
+//         name: form.name.value,
+//         email: form.email.value,
+//         gender:form.gender.value,
+//         DOB:form.DOB.value,
+//         password:form.password.value
+//     }
+//     console.log(jsonObject)
 
-    fetch('http://localhost:3000/people', {
-        method:"POST",
-        body: JSON.stringify(json),
-        headers:
-        {"Content-Type":"application/json"}
-    })
-    Promise.then(response=>{
-        console.log(response)
-    })
+
+//     data is fetched from endpoint 
+//     fetch('http://localhost:3000/people', {
+//         method:"POST",
+//         body: JSON.stringify(json),
+//         headers:
+//         {"Content-Type":"application/json"}
+//     })
+//     Promise.then(response=>{
+//         console.log(response)
+//     })
 
     
 
- })
+//  })
